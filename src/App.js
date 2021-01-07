7b3413f9c18d76dee7c945f9210a5b06d72c4021
Muniba Waqar <muniba.wq@gmail.com>
2021-01-06 20:17:19 -0700

    Creating components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import Accordion from "./components/Accordion/Accordion";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Widgets App</h1>
+      <Accordion />
+    </div>
+  );
 }
 
 export default App;
